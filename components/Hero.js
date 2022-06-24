ce26e771cbac10b855df4d99d33120c25b2c5e47
PREM KUMAR <premenaga@gmail.com>
2022-06-24 10:40:20 +0530

    photo uploaded

--- components/Hero.js
+++ components/Hero.js
@@ -3,17 +3,22 @@ import Button from "./Button";
 
 function Hero() {
   return (
-    <div className="flex flex-col w-[52%] pl-[10%] pt-[7%]">
-      <p className="text-cta text-[20px]"> Hi , my name is </p>
-      <h1 className="text-[72px] font-bold leading-[98px] text-[white] tracking-wider">
-        PREM KUMAR S
-      </h1>
-      <p className="text-[#B0B5BC] text-[30px]">
-        I'm a Computer Science student currently focusing in building and
-        designing digital experiences.
-      </p>
-      <div className="mt-[30px]">
-        <Button buttonName="CONTACT" />
+    <div className="flex">
+      <div className="flex flex-col w-[52%] pl-[10%] pt-[7%]">
+        <p className="text-cta text-[20px]"> Hi , my name is </p>
+        <h1 className="text-[72px] font-bold leading-[98px] text-[white] tracking-wider">
+          PREM KUMAR S
+        </h1>
+        <p className="text-[#B0B5BC] text-[30px]">
+          I'm a Computer Science student currently focusing in building and
+          designing digital experiences.
+        </p>
+        <div className="mt-[30px]">
+          <Button buttonName="CONTACT" />
+        </div>
+      </div>
+      <div className="flex w-[40%]">
+        <img className="w-full h-auto" src="./myphotos/1.png" alt="prem" />
       </div>
     </div>
   );
